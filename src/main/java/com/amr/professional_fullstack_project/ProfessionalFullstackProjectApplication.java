86c323451f7da02750c9bf6958536ab77887204d
amrhefny1987 <amr.hefny@factoriaf5.org>
2024-09-22 14:17:03 +0200

    first commit

---
+++ src/main/java/com/amr/professional_fullstack_project/ProfessionalFullstackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amr.professional_fullstack_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalFullstackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalFullstackProjectApplication.class, args);
+	}
+
+}
